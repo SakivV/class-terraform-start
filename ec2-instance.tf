6b97120d29d7d5e4f63ef31061450b405c3f0f5f
vikas <banagevikas@gmail.com>
2022-12-21 17:23:22 +0100

    create ec2

---
+++ ec2-instance.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.20"
+    }
+  }
+}
+provider "aws" {
+  region  = "us-east-1"
+}
+resource "aws_instance" "myfirstinstance" {
+  instance_type = "t2.micro"
+  ami = "ami-0b5eea76982371e91"
+}
+
